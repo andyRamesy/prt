69e87721ff2beb1706c85c97c31eeb0b9141cde8
andyRamesy <andyramesy@outlook.fr>
2023-07-03 23:19:19 +0300

    start three

--- src/components/canvas/Computers.tsx
+++ src/components/canvas/Computers.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React, { Suspense, useEffect, useState } from "react";
+import { Canvas } from "@react-three/fiber";
+import { OrbitControls, Preload, useGLTF } from "@react-three/drei";
+
+import CanvasLoader from "../Loader";
 
 const Computers = () => {
+  const computer = useGLTF("./destop_pc/scene.gltf")
   return (
-    <div>Computers</div>
+    <mesh>
+      <hemisphereLight intensity={}/>
+    </mesh>
   )
-}
+};
 
-export default Computers
+export default Computers;
