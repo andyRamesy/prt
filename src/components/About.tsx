d8aa7e66bffe0fbf4a0c5bfb900da349685d9da1
andyRamesy <andyramesy@outlook.fr>
2023-07-05 17:56:55 +0300

    startabout page

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,22 +1,28 @@
 import React from "react";
-import Tilt from "react-tilt";
+import { Tilt } from "react-tilt";
 import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../motion";
 
-const ServiceCard = ({}:any) => {
+const ServiceCard = ({ index, title, icon }: any) => {
   return (
-    <>
-    {/* <Tilt className="xs:w-[250px] w-full">
-      <motion.div>
-
+    <Tilt className="xs:w-[250px] w-full">
+      <motion.div
+        variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
+        className="w-full red-pink-gradient  rounded-[20px] shadow-card"
+      >
+        <div
+        
+          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
+        >
+          <img src={icon} alt={title} />
+        </div>
       </motion.div>
-    </Tilt> */}
-    </>
-  )
-}
+    </Tilt>
+  );
+};
 
 const About = () => {
   return (
@@ -35,11 +41,9 @@ const About = () => {
         Libero.
       </motion.p>
       <div className="mt-20 flex flex-wrap gap-10">
-        {
-          services.map((service, index) => (
-            <ServiceCard key={service.title} index={index} {...service}/>
-          ))
-        }
+        {services.map((service, index) => (
+          <ServiceCard key={service.title} index={index} {...service} />
+        ))}
       </div>
     </>
   );
