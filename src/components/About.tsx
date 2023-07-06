71a86152c04c556ffbf285c86696a479b525939d
andyRamesy <andyramesy@outlook.fr>
2023-07-06 20:15:31 +0300

    rebase

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -13,11 +13,11 @@ const ServiceCard = ({ index, title, icon }: any) => {
         variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
         className="w-full red-pink-gradient  rounded-[20px] shadow-card"
       >
-        <div
-        
-          className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
-        >
-          <img src={icon} alt={title} />
+        <div className="bg-tertiary rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col">
+          <img src={icon} alt={title} className="w-16 h-16 object-contain" />
+          <h3 className="text-white text-[20px] font-bold text-center">
+            {title}
+          </h3>
         </div>
       </motion.div>
     </Tilt>
