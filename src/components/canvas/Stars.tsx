89c3a6bcfb65cc32fe1b3b4a9964275f0389cc39
andyRamesy <andyramesy@outlook.fr>
2023-07-19 20:35:03 +0300

    finish star

--- src/components/canvas/Stars.tsx
+++ src/components/canvas/Stars.tsx
@@ -1,24 +1,47 @@
-import React, { Suspense } from "react";
+// @ts-ignore
+import React, { useRef, Suspense, useState } from "react";
 import { Points, PointMaterial, Preload } from "@react-three/drei";
 import CanvasLoader from "../Loader";
 import { Canvas, useFrame } from "@react-three/fiber";
-// import * as from "maath/random/dist/maath-random.esm"
+import * as random from "maath/random/dist/maath-random.esm";
 
-const Stars = () => {
-  return <div>Stars</div>;
+const Stars = (props: any) => {
+  const ref: any = useRef();
+  const [sphere] = useState(() =>
+    random.inSphere(new Float32Array(5000), { radius: 1.5 })
+  );
+
+  useFrame((state, delta) => {// make stars mov
+    ref.current.rotation.x -= delta / 20;
+    ref.current.rotation.y -= delta / 20;
+  });
+
+  return (
+    <group rotation={[0, 0, Math.PI / 4]}>
+      <Points ref={ref} positions={sphere} stride={3} frustumCulled {...props}>
+        <PointMaterial
+          transparent
+          color="#ffa0e0"
+          size={0.001}
+          sizeAttenuation={true}
+          depthWrite={false}
+        />
+      </Points>
+    </group>
+  );
 };
 
 const StarCanvas = () => {
-  return(
-    <div className="w-full h-full absolute inset-0 z-[-1]">
-      <Canvas camera={{position: [0,0,1]}}>
+  return (
+    <div className="w-full h-auto absolute inset-0 z-[-1]">
+      <Canvas camera={{ position: [0, 0, 1] }}>
         <Suspense fallback={null}>
-          <Stars/>
+          <Stars />
         </Suspense>
-        <Preload all/>
+        <Preload all />
       </Canvas>
     </div>
-  )
-}
+  );
+};
 
-export default Stars;
+export default StarCanvas;
