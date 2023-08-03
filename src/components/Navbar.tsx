215a32debff93cf2c8b50e7df25b86b6fb153590
andyRamesy <andyramesy@outlook.fr>
2023-08-04 00:04:32 +0300

    finish sidenav mobile

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,12 +1,25 @@
-import { useState } from "react";
+import React, { MouseEventHandler, useState } from "react";
 import { Link } from "react-router-dom";
 import { styles } from "../styles";
 import { navLinks } from "../constants";
 import { logo, menu, close } from "../assets";
+import "./styles/NavStyleMobile.css";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
+import Fade from "@mui/material/Fade";
 
 const Navbar = () => {
   const [active, setActive] = useState("");
   const [toggle, setToggle] = useState(false);
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+  const open = Boolean(anchorEl);
+  const handleClick = (event: any) => {
+    setAnchorEl(event.currentTarget);
+  };
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+  function closeNav() {}
 
   return (
     <nav
@@ -47,10 +60,37 @@ const Navbar = () => {
             src={toggle ? close : menu}
             alt=""
             className="w-[28px] h-[18px] object-contain cursor-pointer"
+            onClick={handleClick}
+          />
+          <Menu
+            id="fade-menu"
+            MenuListProps={{
+              "aria-labelledby": "fade-button",
+            }}
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+          >
+            {navLinks.map((link) => (
+              <MenuItem onClick={handleClose}>
+                <Link
+                  key={link.id}
+                  to={`/${link.id}`}
+                >
+                  <span className="text-white">{link.title}</span>
+                </Link>
+              </MenuItem>
+            ))}
+          </Menu>
+        </div>
+        {/* <div className="sm:hidden flex flex-1 justify-end items-center">
+          <img
+            src={toggle ? close : menu}
+            alt=""
+            className="w-[28px] h-[18px] object-contain cursor-pointer"
             onClick={() => setToggle(!toggle)}
           />
-          
-          {/* <div
+          <div
             className={`${
               !toggle ? "hidden" : "flex"
             } p-6 black-gradient absolute top-20 right-0 mx-4 my-2 min-w-[140px] z-10 rounded-xl`}
@@ -71,8 +111,8 @@ const Navbar = () => {
                 </li>
               ))}
             </ul>
-          </div> */}
-        </div>
+          </div>
+        </div> */}
       </div>
     </nav>
   );
